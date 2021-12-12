37de50e564587fb2428ba363ccc2cf34a28fa598
wangyelei <wylyzyk@qq.com>
2021-12-12 22:01:23 +0800

    解决https接口无法访问

--- src/network/request.js
+++ src/network/request.js
@@ -1,10 +1,11 @@
 import axios from "axios";
 
+const url = process.env.VUE_APP_URL;
+
 export function request(config) {
   // 1. 创建axios的实例
   const instance = axios.create({
-    // baseURL: "http://adi-v3.dev"
-    baseURL: "http://152.136.185.210:8000/api/w6",
+    baseURL: url,
     // baseURL: "http://152.136.185.210:7878/api/m5/",
     // baseURL: "http://123.207.32.32:8000",
     timeout: 3000
